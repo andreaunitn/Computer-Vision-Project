1641933665dd1a755311baf229cf4bd486589257
Andrea Tomasoni <andrea.tomasoni@tuni.fi>
2024-09-02 19:14:13 +0200

    Fixed Giulia

--- CVProject/Assets/Scripts/CameraController.cs
+++ CVProject/Assets/Scripts/CameraController.cs
@@ -6,11 +6,14 @@ public class CameraController : MonoBehaviour
     public GameObject camera1;
     public GameObject camera2;
     
-    public float camDistance = 0.03f;
+    public float camDistance = 0.08f;
 
     void Start()
     {
        SetCameraPosition();
+       
+       camera1.SetActive(false);
+       camera1.SetActive(true);
     }
     
     void Update()
@@ -19,7 +22,7 @@ public class CameraController : MonoBehaviour
     [Button]
     void SetCameraPosition()
     {
-        camera1.transform.localPosition = transform.localPosition + new Vector3(camDistance / 2, 0f, 0f);
-        camera2.transform.localPosition = transform.localPosition + new Vector3(-camDistance / 2, 0f, 0f);
+        camera1.transform.localPosition = new Vector3(camDistance / 2, 0f, 0f);
+        camera2.transform.localPosition = new Vector3(-camDistance / 2, 0f, 0f);
     }
 }
